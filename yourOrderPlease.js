a2d1974f9d8aa86ac46dc0e016005717b7ec2c3f
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-10-17 18:52:46 -0400

    submit

---
+++ yourOrderPlease.js
@@ -0,0 +1,6 @@
+function order(words){
+  // ...
+  return words.length === 0 ? "" : words.split(' ').sort((a, b) => {
+    return (Number(a.match(/\d/)) - Number(b.match(/\d/)));
+  }).join(' ');
+}

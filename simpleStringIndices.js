da267729c9a867854976b04e0b9bf58c6afbbb65
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-11-04 18:33:47 -0400

    submit

---
+++ simpleStringIndices.js
@@ -0,0 +1,23 @@
+function solve(str,idx){
+  let count = 0;
+  let result = 0;
+
+  if (/[^(]/.test(str[idx])) {
+    return -1;
+  }
+  
+  for (let i = idx; i < str.length; i += 1) {
+    if (str[i] === "(") {
+      count += 1;
+      
+    } else if (str[i] === ")") {
+      count -= 1;
+    }
+    
+    if (count === 0) {
+      result += i;
+      
+      return result;
+    }
+  }
+}

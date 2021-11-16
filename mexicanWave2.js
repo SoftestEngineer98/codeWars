3f959fee0689daab52270259052a1d6715ddd84f
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-11-16 18:12:30 -0500

    submit

---
+++ mexicanWave2.js
@@ -0,0 +1,12 @@
+// Answer 2
+function wave(str){
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let copy = str.split('');
+    if(copy[i] !== ' ') {
+    copy[i] = copy[i].toUpperCase()
+    newArr.push(copy.join(''))
+    }
+  }
+  return newArr
+}

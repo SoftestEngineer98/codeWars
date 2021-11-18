e19113faa284ae5343134ef7e815ff22382f6726
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-11-18 18:09:47 -0500

    submit

---
+++ findTheOddInt4.js
@@ -0,0 +1,12 @@
+function findOdd(A) {
+  //happy coding!
+  
+  for (let i = 0; i <= A.length-1; i += 1) {
+    
+    let elmLength = A.filter((elm) => elm === A[i]).length;
+    
+    if (elmLength % 2 !== 0) {
+      return A[i];
+    }
+  }
+}

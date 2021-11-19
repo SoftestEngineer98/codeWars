9f74aea52dcd95b5169fff456be13c8d9bb799a2
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-11-19 18:07:37 -0500

    submit

---
+++ findTheOddInt2.js
@@ -0,0 +1,11 @@
+// Answer 2
+function findOdd(A) {
+  var obj = {};
+  A.forEach(function(el){
+    obj[el] ? obj[el]++ : obj[el] = 1;
+  });
+  
+  for(prop in obj) {
+    if(obj[prop] % 2 !== 0) return Number(prop);
+  }
+}

835f8bca4914c32baaaa80b268a59be4ba6d1edb
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-11-26 18:00:21 -0500

    submit

---
+++ sumOfParts.js
@@ -0,0 +1,9 @@
+// Answer 3
+function partsSums(ls) {
+  var result = [];
+  result.push(ls.reduce((a, b) => a + b, 0));
+  for ( i = 0; i < ls.length; i++){
+      result.push((result[result.length-1]) - ls[i] );
+  };
+  return result
+}

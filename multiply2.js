8fe2e576e0fbafb05c7c04de1ec3461b603cafb1
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-12-17 19:33:47 -0500

    submit

---
+++ multiply2.js
@@ -0,0 +1,8 @@
+function multiply(a, b){
+  return a * b
+}
+
+// P: a and b are the parameters; Are a&b numbers or a string? 
+// R: returning a return...
+// E: test cases examples are below
+// Psuedo code: input the return so the code can execute 
---
+++ multiply3.js
@@ -0,0 +1,7 @@
+function multiply(a, b){
+  a * b
+}
+function multiply(a, b){
+ console.log (a * b);
+  return a*b;
+}

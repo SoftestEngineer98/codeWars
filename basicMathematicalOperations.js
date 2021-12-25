d70365640e9a202877cf0321539e92afd6c3b066
SoftestEngineer98 <tarafinahdestine@gmail.com>
2021-12-24 19:16:34 -0500

    submit

---
+++ basicMathematicalOperations.js
@@ -0,0 +1,9 @@
+function basicOp(operation, value1, value2)
+{
+  return eval(value1+operation+value2);
+}
+
+// The eval() function evaluates or executes an argument.
+//    This function take string a it parameter.
+// If the argument is an expression, eval() evaluates the expression. 
+//    If the argument is one or more JavaScript statements, eval() executes the statements.
